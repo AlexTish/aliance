7fa78b99b2a384ba7fa546a4592c1f1f362b38f0
Aleksandra Tsishkouskaya <alesandra.arti@gmail.com>
2025-06-19 15:38:33 +0200

    add: mobile menu functions

--- js/main.js
+++ js/main.js
@@ -2,16 +2,35 @@ const navbar = document.querySelector('.navbar');
 const logo = document.querySelector('.logo-svg use');
 const mMenuToggle = document.querySelector('.mobile-menu-toggle');
 const menu = document.querySelector('.mobile-menu');
+
+const lightModeOn = (event) => {
+    navbar.classList.add('navbar-light');
+    logo.setAttribute('href', 'img/sprite.svg#logo');
+}
+
+const lightModeOff = (event) => {
+    navbar.classList.remove('navbar-light');
+    logo.setAttribute('href', 'img/sprite.svg#logo-light');
+}
+
+const openMenu = (event) => {           //функция открытия меню
+    menu.classList.add('is-open');    // вешает класс is-open
+    mMenuToggle.classList.add('close-menu');
+    document.body.style.overflow='hidden';  //запрет прокрутки сайта под меню
+    lightModeOn();
+};
+const closeMenu = (event) => {           //функция закрытия меню
+    menu.classList.remove('is-open');    // убирает класс is-open
+    mMenuToggle.classList.remove('close-menu');
+    document.body.style.overflow='';  //возвращает прокрутку сайта под меню
+    lightModeOff();
+};
+
 window.addEventListener('scroll', () => {
-    if (this.scrollY > 1){
-        navbar.classList.add('navbar-light');
-        logo.setAttribute('href', 'img/sprite.svg#logo');
-    } else {
-        navbar.classList.remove('navbar-light');
-        logo.setAttribute('href', 'img/sprite.svg#logo-light');
-    }
+    this.scrollY > 1 ? lightModeOn() : lightModeOff(); // if?  else:
 });
-mMenuToggle.addEventListener("click", (event) => {
+mMenuToggle.addEventListener('click', (event) => {
     event.preventDefault();
-    menu.classList.toggle("is-open");
+    //menu.classList.toggle("is-open");
+    menu.classList.contains('is-open') ? closeMenu() : openMenu();
 });
