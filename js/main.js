6885ef95a004042a5884958f9015d726f7b0cc65
Aleksandra Tsishkouskaya <alesandra.arti@gmail.com>
2025-06-29 21:44:30 +0300

    add: adaptive site and sliders

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ const menu = document.querySelector('.mobile-menu');
 const lightModeOn = (event) => {
     navbar.classList.add('navbar-light');
     logo.setAttribute('href', 'img/sprite.svg#logo');
-}
+};
 
 const lightModeOff = (event) => {
     navbar.classList.remove('navbar-light');
     logo.setAttribute('href', 'img/sprite.svg#logo-light');
-}
+};
 
 const openMenu = (event) => {           //функция открытия меню
     menu.classList.add('is-open');    // вешает класс is-open
@@ -33,4 +33,61 @@ mMenuToggle.addEventListener('click', (event) => {
     event.preventDefault();
     //menu.classList.toggle("is-open");
     menu.classList.contains('is-open') ? closeMenu() : openMenu();
+});
+
+const swiper = new Swiper('.swiper', {   // 39 https://swiperjs.com/swiper-api#initialize-swiper и https://swiperjs.com/swiper-api#navigation
+    speed: 400,
+    autoHeight: true,
+    // loop: true,   
+    slidesPerView: 5,
+    navigation: {
+        nextEl:'.slider-button-next',
+        prevEl: '.slider-button-prev',
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            //centeredSlides: true,  //размещение слайдера по центру и видимость половины второго слайда
+            //slidesOffsetBefore: -100, // смещение слайдера
+
+        },
+        576: {
+            slidesPerView: 2,
+        },
+        // when window width is >= 480px
+        768: {
+            slidesPerView: 3,
+        },
+        // when window width is >= 640px
+        1024: {
+            slidesPerView: 4,
+        },
+        1200: {
+            slidesPerView: 5,
+        },
+    },
+});
+const swiperSteps = new Swiper('.swiper-steps', {
+    speed: 400,
+    autoHeight: true,
+    slidesPerView: 4, 
+    navigation: {
+        nextEl: '.steps-button-next',
+        prevEl: '.steps-button-prev',
+    },
+    breakpoints: {
+        300: {
+            slidesPerView: 1,
+        },
+        576: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        1024: {
+            slidesPerView: 4,
+        },
+    },
 });
