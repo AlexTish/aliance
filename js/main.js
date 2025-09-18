4a2ce749ec08a73011578f9c5f1b68c919c5fcd6
Aleksandra Tsishkouskaya <alesandra.arti@gmail.com>
2025-09-18 22:31:57 +0200

    fix: modal switch

--- js/main.js
+++ js/main.js
@@ -124,32 +124,21 @@ const swiperBlog = new Swiper('.blog-slider', {
 });
 
 const modal = document.querySelector('.modal');
-const modalToggle = document.querySelectorAll('[data-toggle=modal]')
-const modalClose = document.querySelector('.modal-close')
-console.log(modalToggle);
-modalToggle.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault();
-        modal.classList.add('is-open')
-    });
-});
-modalClose.addEventListener('click', (event) => {
-    event.preventDefault();
-    closeModal();
-});
-// Закрытие модального окна при клике вне его области
-modal.addEventListener('click', (event) => {
-    if (event.target === modal) {
-        closeModal();
-    }
-});
-// Закрытие модального окна при нажатии Escape
-document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape' && modal.classList.contains('is-open')) {
-        closeModal();
-    }
-});
+const modalDialog = document.querySelector('.modal-dialog');
 
-function closeModal() {
-    modal.classList.remove('is-open');
-}
+document.addEventListener('click', (event) => {
+    if (
+        event.target.dataset.toggle === 'modal' || 
+        event.target.parentNode.dataset.toggle === 'modal' ||
+        (!event.composedPath().includes(modalDialog) && 
+        modal.classList.contains('is-open'))
+    ) {
+        event.preventDefault();
+        modal.classList.toggle('is-open');
+    }
+});
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'Escape' && modal.classList.contains('is-open')) {
+        modal.classList.toggle('is-open');
+    }
+});
