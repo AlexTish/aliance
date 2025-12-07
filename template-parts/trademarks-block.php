1bb04e575e467f381ebad4f5ff4b042733aefe19
Aleksandra Tsishkouskaya <alesandra.arti@gmail.com>
2025-12-07 13:06:12 +0100

    fix: code correction after the validator

--- template-parts/trademarks-block.php
+++ template-parts/trademarks-block.php
@@ -14,7 +14,7 @@
                     <h3 class="brand-title">Автохимия AG-Tech</h3>
                     <div class="brand-text">Для современного мира разбавленное изрядной долей эмпатии, рациональное мышление создаёт предпосылки для поставленных обществом задач.</div>
                 </div>
-                <div href="#" class="brand">
+                <div class="brand">
                     <svg class="logo-brand ap" width="180" height="180">
                         <use href="img/sprite.svg#ap"></use>
                     </svg>
